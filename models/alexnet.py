320b747f216d115af5fa9d5987606acdd9a0cf99
pmdaly <pmdaly12@gmail.com>
2020-07-30 17:28:05 +0000

    trainer works with AlexNet

--- models/alexnet.py
+++ models/alexnet.py
@@ -24,38 +24,13 @@ class AlexNet(nn.Module):
                 nn.MaxPool2d(kernel_size=3, stride=2)
                 )
         self.classifier = nn.Sequential(
+                nn.Dropout(p=0.5),
                 nn.Linear(9216, 4096),
+                nn.ReLU(),
                 nn.Dropout(p=0.5),
                 nn.Linear(4096, 4096),
-                nn.Dropout(p=0.5),
+                nn.ReLU(),
                 nn.Linear(4096, 1000)
-                )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-        return x
-
-class Net(nn.Module):
-
-    def __init__(self):
-        super().__init__()
-        self.features = nn.Sequential(
-                nn.Conv2d(3, 16, 3),
-                nn.ReLU(),
-                nn.MaxPool2d(2, 2),
-                nn.Conv2d(16, 64, 3),
-                nn.ReLU(),
-                nn.MaxPool2d(2, 2),
-                nn.Conv2d(64, 128, 3),
-                nn.ReLU(),
-                nn.MaxPool2d(2, 2)
-                )
-        self.classifier = nn.Sequential(
-                nn.Linear(512,128),
-                nn.Linear(128, 64),
-                nn.Linear(64, 10)
                 )
 
     def forward(self, x):
@@ -67,26 +42,25 @@ class Net(nn.Module):
 
 if __name__ == '__main__':
     import torch.optim as optim
-    import torchvision.transforms as transforms
+    from torchvision import transforms
     from train import Trainer
 
-    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    device = torch.device('cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    #model = AlexNet()
-    model = Net()
+    model = AlexNet()
     model.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters())
 
     transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(227),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(10),
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
 
     trainer = Trainer(device=device)
     trainer.load_data(transform=transform, dataset='cifar10')
-    trainer.train(model, criterion, optimizer, n_epochs=30)
+    trainer.train(model, criterion, optimizer, n_epochs=1)
     trainer.test(model, criterion)
